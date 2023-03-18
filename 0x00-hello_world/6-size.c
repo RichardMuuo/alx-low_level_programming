8c395675a8c6cbf08714ec0710f5a56a36be357f
RichardMuuo <rmkyalo21@gmail.com>
2023-03-18 02:17:01 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
-* main prints the sizes of datatypes using sizeof() operator
+* main - prints the sizes of datatypes using sizeof() operator
 *
-* return 0 
+*return 0
 */
 
 int main(void)
-{       
-char c; 
-int i;  
+{
+char c;
+int i; 
 long int li;
 long long int lli;
 float f;
-        
+
 printf("Size of char:%lu.\n", (unsigned long)sizeof(c));
 printf("size of int:%lu.\n", (unsigned long)sizeof(i));
 printf("size of long int:%lu.\n", (unsigned long)sizeof(li));
