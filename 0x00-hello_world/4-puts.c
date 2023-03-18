3489e2277e6eb13bceccbe7a71be38be247b8a40
Richard Muuo <rmkyalo21@gmail.com>
2023-03-18 00:38:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,9 @@
  * Return: Always 0
  */
 
-int main (void)
+int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle\n");
-                return (0);
+puts("\"Programming is like building a multilingual puzzle);
+return (0);
 
 }
