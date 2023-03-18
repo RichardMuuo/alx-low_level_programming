a293a62c71a18019f0414647ea0f7e77e1438132
RichardMuuo <rmkyalo21@gmail.com>
2023-03-18 01:49:15 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+
+printf ("Size of char:%lu.\n",(unsigned long)sizeof(c));
+printf ("size of int:%lu.\n",(unsigned long)sizeof(i));
+printf ("size of long int:%lu.\n",(unsigned long)sizeof(li));
+printf ("size of long long int:%lu.\n",(unsigned long)sizeof(lli));
+printf ("size of float:%lu.\n",(unsigned long)sizeof(f));
+
+return (0);
+
+}
