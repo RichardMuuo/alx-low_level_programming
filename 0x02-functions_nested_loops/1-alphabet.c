e2404c165158be7aba363cae99a5720100d9aae0
RichardMuuo <rmkyalo21@gmail.com>
2023-03-21 06:32:48 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lower case
+ */
+
+void print_alphabet_x10(void);
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
